439a3c5a15803d5a57d09c58cfe8cee1d7881ae1
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-01-02 10:09:44 -0500

    1/2

---
+++ vite-project/main.js
@@ -0,0 +1,12 @@
+
+const URL =`https://emojihub.yurace.pro/api/random`;
+
+async function getData(URL) {
+    let response = await fetch(URL);
+    let data = await response.json();
+    console.log(data);
+    document.querySelector('h1').insertAdjacentHTML(
+        'afterbegin',`<div><h1> ${data.name} ${data.htmlCode} </h1></div`
+    )
+}
+getData(URL);
