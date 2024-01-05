c6b3c44e5b4fdcf58dae6ee5f23c11eb276c5191
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-01-05 10:08:29 -0500

    1/5

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,12 +1,18 @@
 
-const URL =`https://emojihub.yurace.pro/api/random`;
+const URL =`https://emojihub.yurace.pro/api/random/group/face-positive`;
 
 async function getData(URL) {
     let response = await fetch(URL);
     let data = await response.json();
     console.log(data);
     document.querySelector('h1').insertAdjacentHTML(
-        'afterbegin',`<div><h1> ${data.name} ${data.htmlCode} </h1></div`
+        'afterbegin',`<div><h1> ${data.htmlCode[0]} </h1></div`
     )
 }
 getData(URL);
+
+const URL1 =`https://emojihub.yurace.pro/api/random/category/food-and-drink`;
+getData(URL1);
+
+const URL2 =`https://emojihub.yurace.pro/api/random/category/activities`;
+getData(URL2);
